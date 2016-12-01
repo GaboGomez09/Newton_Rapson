8bbec2214a141d06f13929de357f264646693ce3
GaboGomez09 <gomezgabriel09@gmail.com>
2016-12-01 16:05:27 -0600

    Trabajando en el try catch e intentando hacer que la division por cero lanze una excpecion

--- src/practicasiete/PracticaSiete.java
+++ src/practicasiete/PracticaSiete.java
@@ -14,11 +14,9 @@ public class PracticaSiete{
       derivador.setFuncionPolinomial();
       System.out.println("F(x): "+Arrays.toString(derivador.getFuncionPolinomial()));
       System.out.println("F'(x): "+Arrays.toString(derivador.Derivar()));
-      double x = 3.0;
-      double fx = derivador.Evaluar(derivador.getFuncionPolinomial(),x);
-      double dfx = derivador.Evaluar(derivador.Derivar(),x);
-      System.out.println("F("+x+"): "+fx);
-      System.out.println("F'("+x+"): "+dfx);
-      
+      NewtonRaphson nr = new NewtonRaphson(derivador);
+      nr.setIteraciones();
+      nr.setX0();
+      nr.Converge();
     }
 }
